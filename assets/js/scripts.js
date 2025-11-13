7d00f1241f9b580bc0ce4c35681cbec66bb3c926
yeexcalibur <iamceylon11@gmail.com>
2025-11-13 16:45:39 +0800

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('more-btn').onclick = function() {
+  document.getElementById('more-link').style.display = 'block';  // 显示链接
+  document.getElementById('more-btn').style.display = 'none';    // 隐藏按钮
+};
